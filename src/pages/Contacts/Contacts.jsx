b02507c6d6b0cd31f0752e69e2f32827b902d383
HasinIshrakK <hasinishrakk@gmail.com>
2026-02-07 03:40:51 +0600

    feat: Contacts page added

---
+++ src/pages/Contacts/Contacts.jsx
@@ -0,0 +1,76 @@
+export default function Contacts() {
+
+    return (
+        <div className="items-center flex justify-center">
+            <div className="grid gap-3 md:gap-0 grid-cols-1 md:grid-cols-2">
+                <div className="flex justify-center items-center gap-2">
+                    <div className="w-20 md:w-40 h-20 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,_100%_50%,_50%_100%,_0%_50%)]">
+                        <img src="https://preview.redd.it/is-there-a-possibility-that-saitama-will-reconnect-with-his-v0-a68r1dfui7ce1.jpeg?auto=webp&s=af4114caff35705f8bc4fc3a7dd04e39427e4480"
+                            alt="Profile Pic" className="w-full h-full object-cover" />
+                    </div>
+                    <div className="bg-purple-600 rounded-2xl border-2 p-4">
+                        <h1 className="text-2xl">BitBuilder Member</h1>
+                        <p>
+                            Email: bit@builder.com
+                        </p>
+                    </div>
+                </div> <div></div>  <div></div><div className="flex justify-center items-center gap-2">
+                    <div className="bg-purple-600 rounded-2xl border-2 p-4">
+                        <h1 className="text-2xl">BitBuilder Member</h1>
+                        <p>
+                            Email: bit@builder.com
+                        </p>
+                    </div>
+                    <div className="w-20 md:w-40 h-20 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,_100%_50%,_50%_100%,_0%_50%)]">
+                        <img src="https://preview.redd.it/is-there-a-possibility-that-saitama-will-reconnect-with-his-v0-a68r1dfui7ce1.jpeg?auto=webp&s=af4114caff35705f8bc4fc3a7dd04e39427e4480"
+                            alt="Profile Pic" className="w-full h-full object-cover" />
+                    </div>
+                </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2">
+                    <div className="w-20 md:w-40 h-20 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,_100%_50%,_50%_100%,_0%_50%)]">
+                        <img src="https://preview.redd.it/is-there-a-possibility-that-saitama-will-reconnect-with-his-v0-a68r1dfui7ce1.jpeg?auto=webp&s=af4114caff35705f8bc4fc3a7dd04e39427e4480"
+                            alt="Profile Pic" className="w-full h-full object-cover" />
+                    </div>
+                    <div className="bg-purple-600 rounded-2xl border-2 p-4">
+                        <h1 className="text-2xl">BitBuilder Member</h1>
+                        <p>
+                            Email: bit@builder.com
+                        </p>
+                    </div>
+                </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2">
+                    <div className="bg-purple-600 rounded-2xl border-2 p-4">
+                        <h1 className="text-2xl">BitBuilder Member</h1>
+                        <p>
+                            Email: bit@builder.com
+                        </p>
+                    </div>
+                    <div className="w-20 md:w-40 h-20 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,_100%_50%,_50%_100%,_0%_50%)]">
+                        <img src="https://preview.redd.it/is-there-a-possibility-that-saitama-will-reconnect-with-his-v0-a68r1dfui7ce1.jpeg?auto=webp&s=af4114caff35705f8bc4fc3a7dd04e39427e4480"
+                            alt="Profile Pic" className="w-full h-full object-cover" />
+                    </div>
+                </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2">
+                    <div className="w-20 md:w-40 h-20 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,_100%_50%,_50%_100%,_0%_50%)]">
+                        <img src="https://preview.redd.it/is-there-a-possibility-that-saitama-will-reconnect-with-his-v0-a68r1dfui7ce1.jpeg?auto=webp&s=af4114caff35705f8bc4fc3a7dd04e39427e4480"
+                            alt="Profile Pic" className="w-full h-full object-cover" />
+                    </div>
+                    <div className="bg-purple-600 rounded-2xl border-2 p-4">
+                        <h1 className="text-2xl">BitBuilder Member</h1>
+                        <p>
+                            Email: bit@builder.com
+                        </p>
+                    </div>
+                </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2">
+                    <div className="bg-purple-600 rounded-2xl border-2 p-4">
+                        <h1 className="text-2xl">BitBuilder Member</h1>
+                        <p>
+                            Email: bit@builder.com
+                        </p>
+                    </div>
+                    <div className="w-20 md:w-40 h-20 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,_100%_50%,_50%_100%,_0%_50%)]">
+                        <img src="https://preview.redd.it/is-there-a-possibility-that-saitama-will-reconnect-with-his-v0-a68r1dfui7ce1.jpeg?auto=webp&s=af4114caff35705f8bc4fc3a7dd04e39427e4480"
+                            alt="Profile Pic" className="w-full h-full object-cover" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
