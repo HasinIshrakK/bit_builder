76f66c34d942225cec0d7e4289169d216b8142f3
Asha-08 <ashaamina91@gmail.com>
2026-02-07 22:17:23 +0600

    created all projects and project details page

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
+import AllProjects from "../pages/AllProjects/AllProjects";
+import ProjectDetails from "../pages/ProjectDetails/ProjectDetails";
 
 export const router = createBrowserRouter(
     [
@@ -11,7 +13,15 @@ export const router = createBrowserRouter(
                 {
                     index:true,
                     Component:Home
-                }
+                },
+                {
+                    path:"/projects",
+                    Component:AllProjects
+                },
+                {
+                    path:"/projects/:id",
+                    Component:ProjectDetails
+                },
             ]
         }
     ]
