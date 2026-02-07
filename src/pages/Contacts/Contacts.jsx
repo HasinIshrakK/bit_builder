001cad0e9631d73a5ed2913cc0279e16e293baee
HasinIshrakK <hasinishrakk@gmail.com>
2026-02-07 23:25:00 +0600

    Data updated

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -16,68 +16,68 @@ export default function Contacts() {
                 <div className="grid gap-3 md:gap-0 grid-cols-1">
                     <div className="flex justify-center items-center gap-2 md:-ml-32">
                         <div className="w-24 md:w-40 h-24 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,100%_50%,50%_100%,0%_50%)]">
-                            <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/618423505_767006119769338_3397441783340675595_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Q1ja5NQVhnwQ7kNvwE1XzWU&_nc_oc=AdmHbdedj7lJ85-TbOtMDFIKw4xg58VtC6APO0kGQbbgW0c79aLqBHrNaHszafSOaFI&_nc_zt=23&_nc_ht=scontent.fdac7-1.fna&_nc_gid=atpwWIThkUngVqMa79VRFQ&oh=00_Afta3l_qCh74qCkQGb13hdn0XEljJw391vtfPz-0sYW6bg&oe=698C876D"
+                            <img src="https://avatars.githubusercontent.com/u/88761193?v=4"
                                 alt="Profile Pic" className="w-full h-full object-cover" />
                         </div>
                         <div className="bg-purple-900 rounded-2xl p-4">
-                            <h1 className="text-2xl">BitBuilder Member</h1>
+                            <h1 className="text-2xl">Ahmed Abrar Zayad</h1>
                             <p>
-                                Email: bit@builder.com
+                                Email: ahmedabrarzayad@gmail.com
                             </p>
                         </div>
-                    </div> <div></div>  <div></div><div className="flex justify-center items-center gap-2 md:-mr-32 md:-mt-10 text-right">
+                    </div> <div className="flex justify-center items-center gap-2 md:-mr-32 md:-mt-10 text-right">
                         <div className="bg-purple-900 rounded-2xl p-4">
-                            <h1 className="text-2xl">BitBuilder Member</h1>
+                            <h1 className="text-2xl">Mahfuzur Rahman</h1>
                             <p>
-                                Email: bit@builder.com
+                                Email: ripon301252@gmail.com
                             </p>
                         </div>
                         <div className="w-24 md:w-40 h-24 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,100%_50%,50%_100%,0%_50%)]">
-                            <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/618423505_767006119769338_3397441783340675595_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Q1ja5NQVhnwQ7kNvwE1XzWU&_nc_oc=AdmHbdedj7lJ85-TbOtMDFIKw4xg58VtC6APO0kGQbbgW0c79aLqBHrNaHszafSOaFI&_nc_zt=23&_nc_ht=scontent.fdac7-1.fna&_nc_gid=atpwWIThkUngVqMa79VRFQ&oh=00_Afta3l_qCh74qCkQGb13hdn0XEljJw391vtfPz-0sYW6bg&oe=698C876D"
+                            <img src="https://avatars.githubusercontent.com/u/93132951?v=4"
                                 alt="Profile Pic" className="w-full h-full object-cover" />
                         </div>
-                    </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2 md:-ml-32 md:-mt-10">
+                    </div>  <div className="flex justify-center items-center gap-2 md:-ml-32 md:-mt-10">
                         <div className="w-24 md:w-40 h-24 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,100%_50%,50%_100%,0%_50%)]">
-                            <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/618423505_767006119769338_3397441783340675595_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Q1ja5NQVhnwQ7kNvwE1XzWU&_nc_oc=AdmHbdedj7lJ85-TbOtMDFIKw4xg58VtC6APO0kGQbbgW0c79aLqBHrNaHszafSOaFI&_nc_zt=23&_nc_ht=scontent.fdac7-1.fna&_nc_gid=atpwWIThkUngVqMa79VRFQ&oh=00_Afta3l_qCh74qCkQGb13hdn0XEljJw391vtfPz-0sYW6bg&oe=698C876D"
-                                alt="Profile Pic" className="w-full h-full object-cover" />
-                        </div>
-                        <div className="bg-purple-900 rounded-2xl p-4">
-                            <h1 className="text-2xl">BitBuilder Member</h1>
-                            <p>
-                                Email: bit@builder.com
-                            </p>
-                        </div>
-                    </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2 md:-mr-32 md:-mt-10 text-right">
-                        <div className="bg-purple-900 rounded-2xl p-4">
-                            <h1 className="text-2xl">BitBuilder Member</h1>
-                            <p>
-                                Email: bit@builder.com
-                            </p>
-                        </div>
-                        <div className="w-24 md:w-40 h-24 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,100%_50%,50%_100%,0%_50%)]">
-                            <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/618423505_767006119769338_3397441783340675595_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Q1ja5NQVhnwQ7kNvwE1XzWU&_nc_oc=AdmHbdedj7lJ85-TbOtMDFIKw4xg58VtC6APO0kGQbbgW0c79aLqBHrNaHszafSOaFI&_nc_zt=23&_nc_ht=scontent.fdac7-1.fna&_nc_gid=atpwWIThkUngVqMa79VRFQ&oh=00_Afta3l_qCh74qCkQGb13hdn0XEljJw391vtfPz-0sYW6bg&oe=698C876D"
-                                alt="Profile Pic" className="w-full h-full object-cover" />
-                        </div>
-                    </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2 md:-ml-32 md:-mt-10">
-                        <div className="w-24 md:w-40 h-24 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,100%_50%,50%_100%,0%_50%)]">
-                            <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/618423505_767006119769338_3397441783340675595_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Q1ja5NQVhnwQ7kNvwE1XzWU&_nc_oc=AdmHbdedj7lJ85-TbOtMDFIKw4xg58VtC6APO0kGQbbgW0c79aLqBHrNaHszafSOaFI&_nc_zt=23&_nc_ht=scontent.fdac7-1.fna&_nc_gid=atpwWIThkUngVqMa79VRFQ&oh=00_Afta3l_qCh74qCkQGb13hdn0XEljJw391vtfPz-0sYW6bg&oe=698C876D"
+                            <img src="/image.jpg"
                                 alt="Profile Pic" className="w-full h-full object-cover" />
                         </div>
                         <div className="bg-purple-900 rounded-2xl p-4">
-                            <h1 className="text-2xl">BitBuilder Member</h1>
+                            <h1 className="text-2xl">Amina Akther Asha</h1>
                             <p>
-                                Email: bit@builder.com
+                                Email: ashaamina91@gmail.com
                             </p>
                         </div>
-                    </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2 md:-mr-32 md:-mt-10 text-right">
+                    </div>  <div className="flex justify-center items-center gap-2 md:-mr-32 md:-mt-10 text-right">
                         <div className="bg-purple-900 rounded-2xl p-4">
-                            <h1 className="text-2xl">BitBuilder Member</h1>
+                            <h1 className="text-2xl">Faisal</h1>
                             <p>
-                                Email: bit@builder.com
+                                Email: itsfaisalhossen@gmail.com
                             </p>
                         </div>
                         <div className="w-24 md:w-40 h-24 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,100%_50%,50%_100%,0%_50%)]">
-                            <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/618423505_767006119769338_3397441783340675595_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Q1ja5NQVhnwQ7kNvwE1XzWU&_nc_oc=AdmHbdedj7lJ85-TbOtMDFIKw4xg58VtC6APO0kGQbbgW0c79aLqBHrNaHszafSOaFI&_nc_zt=23&_nc_ht=scontent.fdac7-1.fna&_nc_gid=atpwWIThkUngVqMa79VRFQ&oh=00_Afta3l_qCh74qCkQGb13hdn0XEljJw391vtfPz-0sYW6bg&oe=698C876D"
+                            <img src="https://avatars.githubusercontent.com/u/108367521?v=4"
+                                alt="Profile Pic" className="w-full h-full object-cover" />
+                        </div>
+                    </div>  <div className="flex justify-center items-center gap-2 md:-ml-32 md:-mt-10">
+                        <div className="w-24 md:w-40 h-24 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,100%_50%,50%_100%,0%_50%)]">
+                            <img src="https://avatars.githubusercontent.com/u/218386621?v=4"
+                                alt="Profile Pic" className="w-full h-full object-cover" />
+                        </div>
+                        <div className="bg-purple-900 rounded-2xl p-4">
+                            <h1 className="text-2xl">Tahmid Ahmed</h1>
+                            <p>
+                                Email: tahmid7282@gmail.com
+                            </p>
+                        </div>
+                    </div>  <div className="flex justify-center items-center gap-2 md:-mr-32 md:-mt-10 text-right">
+                        <div className="bg-purple-900 rounded-2xl p-4">
+                            <h1 className="text-2xl">MD. HASIN ISHRAK KHAN</h1>
+                            <p>
+                                Email: hasinishrakk@gmail.com
+                            </p>
+                        </div>
+                        <div className="w-24 md:w-40 h-24 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,100%_50%,50%_100%,0%_50%)]">
+                            <img src="https://avatars.githubusercontent.com/u/219306826?v=4"
                                 alt="Profile Pic" className="w-full h-full object-cover" />
                         </div>
                     </div>
