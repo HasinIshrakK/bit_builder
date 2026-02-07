d1335433cafbd1b7c77b7aa8b7c3083a13482ae7
HasinIshrakK <hasinishrakk@gmail.com>
2026-02-07 11:45:47 +0600

    feat: Layout updated

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,73 +1,85 @@
 export default function Contacts() {
 
     return (
-        <div className="items-center flex justify-center">
-            <div className="grid gap-3 md:gap-0 grid-cols-1 md:grid-cols-2">
-                <div className="flex justify-center items-center gap-2">
-                    <div className="w-20 md:w-40 h-20 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,_100%_50%,_50%_100%,_0%_50%)]">
-                        <img src="https://preview.redd.it/is-there-a-possibility-that-saitama-will-reconnect-with-his-v0-a68r1dfui7ce1.jpeg?auto=webp&s=af4114caff35705f8bc4fc3a7dd04e39427e4480"
-                            alt="Profile Pic" className="w-full h-full object-cover" />
-                    </div>
-                    <div className="bg-purple-600 rounded-2xl border-2 p-4">
-                        <h1 className="text-2xl">BitBuilder Member</h1>
-                        <p>
-                            Email: bit@builder.com
-                        </p>
-                    </div>
-                </div> <div></div>  <div></div><div className="flex justify-center items-center gap-2">
-                    <div className="bg-purple-600 rounded-2xl border-2 p-4">
-                        <h1 className="text-2xl">BitBuilder Member</h1>
-                        <p>
-                            Email: bit@builder.com
-                        </p>
-                    </div>
-                    <div className="w-20 md:w-40 h-20 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,_100%_50%,_50%_100%,_0%_50%)]">
-                        <img src="https://preview.redd.it/is-there-a-possibility-that-saitama-will-reconnect-with-his-v0-a68r1dfui7ce1.jpeg?auto=webp&s=af4114caff35705f8bc4fc3a7dd04e39427e4480"
-                            alt="Profile Pic" className="w-full h-full object-cover" />
-                    </div>
-                </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2">
-                    <div className="w-20 md:w-40 h-20 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,_100%_50%,_50%_100%,_0%_50%)]">
-                        <img src="https://preview.redd.it/is-there-a-possibility-that-saitama-will-reconnect-with-his-v0-a68r1dfui7ce1.jpeg?auto=webp&s=af4114caff35705f8bc4fc3a7dd04e39427e4480"
-                            alt="Profile Pic" className="w-full h-full object-cover" />
-                    </div>
-                    <div className="bg-purple-600 rounded-2xl border-2 p-4">
-                        <h1 className="text-2xl">BitBuilder Member</h1>
-                        <p>
-                            Email: bit@builder.com
-                        </p>
-                    </div>
-                </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2">
-                    <div className="bg-purple-600 rounded-2xl border-2 p-4">
-                        <h1 className="text-2xl">BitBuilder Member</h1>
-                        <p>
-                            Email: bit@builder.com
-                        </p>
-                    </div>
-                    <div className="w-20 md:w-40 h-20 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,_100%_50%,_50%_100%,_0%_50%)]">
-                        <img src="https://preview.redd.it/is-there-a-possibility-that-saitama-will-reconnect-with-his-v0-a68r1dfui7ce1.jpeg?auto=webp&s=af4114caff35705f8bc4fc3a7dd04e39427e4480"
-                            alt="Profile Pic" className="w-full h-full object-cover" />
-                    </div>
-                </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2">
-                    <div className="w-20 md:w-40 h-20 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,_100%_50%,_50%_100%,_0%_50%)]">
-                        <img src="https://preview.redd.it/is-there-a-possibility-that-saitama-will-reconnect-with-his-v0-a68r1dfui7ce1.jpeg?auto=webp&s=af4114caff35705f8bc4fc3a7dd04e39427e4480"
-                            alt="Profile Pic" className="w-full h-full object-cover" />
-                    </div>
-                    <div className="bg-purple-600 rounded-2xl border-2 p-4">
-                        <h1 className="text-2xl">BitBuilder Member</h1>
-                        <p>
-                            Email: bit@builder.com
-                        </p>
-                    </div>
-                </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2">
-                    <div className="bg-purple-600 rounded-2xl border-2 p-4">
-                        <h1 className="text-2xl">BitBuilder Member</h1>
-                        <p>
-                            Email: bit@builder.com
-                        </p>
-                    </div>
-                    <div className="w-20 md:w-40 h-20 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,_100%_50%,_50%_100%,_0%_50%)]">
-                        <img src="https://preview.redd.it/is-there-a-possibility-that-saitama-will-reconnect-with-his-v0-a68r1dfui7ce1.jpeg?auto=webp&s=af4114caff35705f8bc4fc3a7dd04e39427e4480"
-                            alt="Profile Pic" className="w-full h-full object-cover" />
+        <div>
+            {/* Header */}
+            <div className="mb-10 text-center">
+                <h1 className="text-3xl font-bold g-linear-to-r text-purple-300">
+                    Contact Info
+                </h1>
+                <p className="mt-2 text-gray-400">
+                    Meet our awesome team and connect with them.
+                </p>
+            </div>
+            {/* Membar cards */}
+            <div className="items-center flex justify-center">
+                <div className="grid gap-3 md:gap-0 grid-cols-1">
+                    <div className="flex justify-center items-center gap-2 md:-ml-32">
+                        <div className="w-24 md:w-40 h-24 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,100%_50%,50%_100%,0%_50%)]">
+                            <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/618423505_767006119769338_3397441783340675595_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Q1ja5NQVhnwQ7kNvwE1XzWU&_nc_oc=AdmHbdedj7lJ85-TbOtMDFIKw4xg58VtC6APO0kGQbbgW0c79aLqBHrNaHszafSOaFI&_nc_zt=23&_nc_ht=scontent.fdac7-1.fna&_nc_gid=atpwWIThkUngVqMa79VRFQ&oh=00_Afta3l_qCh74qCkQGb13hdn0XEljJw391vtfPz-0sYW6bg&oe=698C876D"
+                                alt="Profile Pic" className="w-full h-full object-cover" />
+                        </div>
+                        <div className="bg-purple-900 rounded-2xl p-4">
+                            <h1 className="text-2xl">BitBuilder Member</h1>
+                            <p>
+                                Email: bit@builder.com
+                            </p>
+                        </div>
+                    </div> <div></div>  <div></div><div className="flex justify-center items-center gap-2 md:-mr-32 md:-mt-10 text-right">
+                        <div className="bg-purple-900 rounded-2xl p-4">
+                            <h1 className="text-2xl">BitBuilder Member</h1>
+                            <p>
+                                Email: bit@builder.com
+                            </p>
+                        </div>
+                        <div className="w-24 md:w-40 h-24 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,100%_50%,50%_100%,0%_50%)]">
+                            <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/618423505_767006119769338_3397441783340675595_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Q1ja5NQVhnwQ7kNvwE1XzWU&_nc_oc=AdmHbdedj7lJ85-TbOtMDFIKw4xg58VtC6APO0kGQbbgW0c79aLqBHrNaHszafSOaFI&_nc_zt=23&_nc_ht=scontent.fdac7-1.fna&_nc_gid=atpwWIThkUngVqMa79VRFQ&oh=00_Afta3l_qCh74qCkQGb13hdn0XEljJw391vtfPz-0sYW6bg&oe=698C876D"
+                                alt="Profile Pic" className="w-full h-full object-cover" />
+                        </div>
+                    </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2 md:-ml-32 md:-mt-10">
+                        <div className="w-24 md:w-40 h-24 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,100%_50%,50%_100%,0%_50%)]">
+                            <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/618423505_767006119769338_3397441783340675595_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Q1ja5NQVhnwQ7kNvwE1XzWU&_nc_oc=AdmHbdedj7lJ85-TbOtMDFIKw4xg58VtC6APO0kGQbbgW0c79aLqBHrNaHszafSOaFI&_nc_zt=23&_nc_ht=scontent.fdac7-1.fna&_nc_gid=atpwWIThkUngVqMa79VRFQ&oh=00_Afta3l_qCh74qCkQGb13hdn0XEljJw391vtfPz-0sYW6bg&oe=698C876D"
+                                alt="Profile Pic" className="w-full h-full object-cover" />
+                        </div>
+                        <div className="bg-purple-900 rounded-2xl p-4">
+                            <h1 className="text-2xl">BitBuilder Member</h1>
+                            <p>
+                                Email: bit@builder.com
+                            </p>
+                        </div>
+                    </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2 md:-mr-32 md:-mt-10 text-right">
+                        <div className="bg-purple-900 rounded-2xl p-4">
+                            <h1 className="text-2xl">BitBuilder Member</h1>
+                            <p>
+                                Email: bit@builder.com
+                            </p>
+                        </div>
+                        <div className="w-24 md:w-40 h-24 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,100%_50%,50%_100%,0%_50%)]">
+                            <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/618423505_767006119769338_3397441783340675595_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Q1ja5NQVhnwQ7kNvwE1XzWU&_nc_oc=AdmHbdedj7lJ85-TbOtMDFIKw4xg58VtC6APO0kGQbbgW0c79aLqBHrNaHszafSOaFI&_nc_zt=23&_nc_ht=scontent.fdac7-1.fna&_nc_gid=atpwWIThkUngVqMa79VRFQ&oh=00_Afta3l_qCh74qCkQGb13hdn0XEljJw391vtfPz-0sYW6bg&oe=698C876D"
+                                alt="Profile Pic" className="w-full h-full object-cover" />
+                        </div>
+                    </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2 md:-ml-32 md:-mt-10">
+                        <div className="w-24 md:w-40 h-24 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,100%_50%,50%_100%,0%_50%)]">
+                            <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/618423505_767006119769338_3397441783340675595_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Q1ja5NQVhnwQ7kNvwE1XzWU&_nc_oc=AdmHbdedj7lJ85-TbOtMDFIKw4xg58VtC6APO0kGQbbgW0c79aLqBHrNaHszafSOaFI&_nc_zt=23&_nc_ht=scontent.fdac7-1.fna&_nc_gid=atpwWIThkUngVqMa79VRFQ&oh=00_Afta3l_qCh74qCkQGb13hdn0XEljJw391vtfPz-0sYW6bg&oe=698C876D"
+                                alt="Profile Pic" className="w-full h-full object-cover" />
+                        </div>
+                        <div className="bg-purple-900 rounded-2xl p-4">
+                            <h1 className="text-2xl">BitBuilder Member</h1>
+                            <p>
+                                Email: bit@builder.com
+                            </p>
+                        </div>
+                    </div>  <div></div>  <div></div> <div className="flex justify-center items-center gap-2 md:-mr-32 md:-mt-10 text-right">
+                        <div className="bg-purple-900 rounded-2xl p-4">
+                            <h1 className="text-2xl">BitBuilder Member</h1>
+                            <p>
+                                Email: bit@builder.com
+                            </p>
+                        </div>
+                        <div className="w-24 md:w-40 h-24 md:h-40 overflow-hidden [clip-path:polygon(50%_0%,100%_50%,50%_100%,0%_50%)]">
+                            <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/618423505_767006119769338_3397441783340675595_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Q1ja5NQVhnwQ7kNvwE1XzWU&_nc_oc=AdmHbdedj7lJ85-TbOtMDFIKw4xg58VtC6APO0kGQbbgW0c79aLqBHrNaHszafSOaFI&_nc_zt=23&_nc_ht=scontent.fdac7-1.fna&_nc_gid=atpwWIThkUngVqMa79VRFQ&oh=00_Afta3l_qCh74qCkQGb13hdn0XEljJw391vtfPz-0sYW6bg&oe=698C876D"
+                                alt="Profile Pic" className="w-full h-full object-cover" />
+                        </div>
                     </div>
                 </div>
             </div>
